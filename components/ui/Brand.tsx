c6b7c0365eafdda99a3c53d0ff3bf95e5b6f8581
rasyajusticio <rasyajusticio.public@gmail.com>
2025-01-17 16:37:33 +0700

    feat: add brand component

---
+++ components/ui/Brand.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Oxygen } from "next/font/google";
+
+const oxygen = Oxygen({
+  variable: "--font-oxygen",
+  subsets: ["latin"],
+  weight: ["300", "400", "700"],
+});
+
+const Brand = () => {
+  return (
+    <div
+      className={`${oxygen.className} flex items-center justify-center w-fit text-2xl`}
+    >
+      <p className="text-emerald-400">Jscio</p>
+      <p>store</p>
+    </div>
+  );
+};
+
+export default Brand;
