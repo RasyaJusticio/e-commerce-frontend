13773df798de62a39e0a1b11fb8fe56e599e28b7
rasyajusticio <rasyajusticio.public@gmail.com>
2025-01-17 16:29:16 +0700

    feat(auth): make login schema for login form validation

--- features/auth/index.ts
+++ features/auth/index.ts
@@ -1,2 +1,6 @@
+// Schemas
+export { loginSchema } from "./schemas/loginSchema";
+export type { LoginSchema } from "./schemas/loginSchema";
+
 // Utils
 export { isLoggedIn } from './utils/isLoggedIn'
