4125ecf65d177acffba2a87f42074393d7b3fc1b
RasyaJusticio <rasyajusticio.personal@gmail.com>
2025-02-05 13:50:47 +0700

    feat: add a top loading bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NextTopLoader from "nextjs-toploader";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,8 +26,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
+        className={`${geistSans.variable} ${geistMono.variable} dark antialiased`}
       >
+        <NextTopLoader
+          color="#10b77f"
+        />
         {children}
       </body>
     </html>
